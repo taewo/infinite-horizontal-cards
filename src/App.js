f870f6cad4d6ebf148f53c6d79e9a937c6dc6475
Taewoong Moon <moondingie5@gmail.com>
2018-04-19 23:15:44 +0900

    상단의 태그의 flex 문제 발견

--- src/App.js
+++ src/App.js
@@ -2,20 +2,34 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    const elements = document.getElementsByClassName("card--content")
+    console.log('elements....', elements);
+    for (let i = 0; i < elements.length; i += 1) {
+      elements[i].addEventListener('mouseenter', function(e) {
+        console.log('i..', i)
+        // elements[i].style.width = "400px"
+        // elements[i].style.height = "400px"
+        elements[i].style.WebkitTransition = "all 3s"
+      })
+    }
+  }
+
   render() {
     return (
       <div>
-        <section class="card">
-          <div class="card--content"></div>
-          <div class="card--content"></div>
-          <div class="card--content"></div>
-          <div class="card--content"></div>
-          <div class="card--content"></div>
-          <div class="card--content"></div>
-          <div class="card--content"></div>
-          <div class="card--content"></div>
-          <div class="card--content"></div>
-          <div class="card--content"></div>
+        <section className="card">
+          <div className="card--content"></div>
+          <div className="card--content"></div>
+          <div className="card--content"></div>
+          <div className="card--content"></div>
+          <div className="card--content"></div>
+          <div className="card--content"></div>
+          <div className="card--content"></div>
+          <div className="card--content"></div>
+          <div className="card--content"></div>
+          <div className="card--content"></div>
         </section>
       </div>
     );
